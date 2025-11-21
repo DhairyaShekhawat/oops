7a6783cbdfadbd7e7a82cd940535c2139ce8e48d
DhairyaShekhawat <dhairyeshekhawat@gmail.com>
2025-11-21 11:21:30 +0530

    Update isprime file with comments

---
+++ src/oops/elementary_sheet_no2/question_1_isprime.java
@@ -0,0 +1,12 @@
+package oops.elementary_sheet_no2;
+
+public class question_1_isprime {
+    //mvfyusadzv h svduofdzcx
+    //sflkgasizxcjksiosdzv
+    //Xcklcbfiuaedvzcujkgsuidvbsv
+    //zcklfbuiadvckjcsddofvZ
+    //cLSJGDYIFKAJVGUOGALDFBOADDCVCV
+    //ADFJADDGCUVALKDFBIOSV
+    //ADJKGASIUCJKLASGIFUADSKHSFGF
+    System.out.println("Hello, World!");
+}
